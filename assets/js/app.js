a0abbcbd2ce4c1f0cef18dbbb696d6b4bf3d54d7
lucperkins <lucperkins@gmail.com>
2019-11-22 13:02:53 -0800

    Add navbar plus social links

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+function navbarMenu() {
+  const burger = $(".navbar-burger"),
+        menu = $(".navbar-menu");
+
+  burger.click(() => {
+    [burger, menu].forEach((el) => el.toggleClass('is-active'));
+  });
+}
+
+$(function() {
+  navbarMenu();
+});
