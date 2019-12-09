c27ed0d115a723dc6afeb7c65cf759717ba02fcf
lucperkins <lucperkins@gmail.com>
2019-12-09 11:35:13 -0800

    Add initial Fuse.js search implementation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,3 @@
-{{ $indexUrl := printf "%s/index.json" .Lang | relURL }}
 const navbarMenu = () => {
   const burger = $(".navbar-burger"),
         menu = $(".navbar-menu");
@@ -8,52 +7,6 @@ const navbarMenu = () => {
   });
 }
 
-const search = () => {
-  const searchBar = $('#search-bar');
-
-  const options = {
-    shouldSort: true,
-    threshold: 0.6,
-    maxPatternLength: 32,
-    minMatchCharLength: 1,
-    keys: [
-      {
-        name: 'title',
-        weight: 0.7
-      },
-      {
-        name: 'summary',
-        weight: 0.5,
-      },
-      {
-        name: 'body',
-        weight: 0.3
-      }
-    ]
-  }
-
-  const jsonify = (res) => {
-    return res.json();
-  }
-
-  const showSearchDrawer = () => {
-    searchBar.hide();
-  }
-
-  const searchBarFocus = () => {
-    showSearchDrawer();
-  }
-
-  const handleIndex = (idx) => {
-    searchBar.on('focus', searchBarFocus);
-  }
-
-  fetch('{{ $indexUrl }}')
-    .then(jsonify)
-    .then(handleIndex);
-}
-
-$(function() {
+$(() => {
   navbarMenu();
-  search();
 });
