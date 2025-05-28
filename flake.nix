216059233e2838d78553c1835fc5b7ae628e5cab
Luc Perkins <lucperkins@gmail.com>
2025-05-28 14:27:29 -0700

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "An empty flake template that you can adapt to your own environment";
+
+  # Flake inputs
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+
+  # Flake outputs
+  outputs = inputs:
+    let
+      # The systems supported for this flake
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default =
+          let
+            script = name: runtimeInputs: text: pkgs.writeShellApplication { inherit name runtimeInputs text; };
+          in
+          pkgs.mkShell {
+            # The Nix packages provided in the environment
+            # Add any you need here
+            packages = with pkgs; [
+              htmltest
+              hugo
+              (script "build" [ hugo ] "hugo --minify")
+              (script "dev" [ hugo ] "hugo server -p 3000 --disableFastRender")
+            ];
+
+            # Set any environment variables for your dev shell
+            env = { };
+
+            # Add any shell logic you want executed any time the environment is activated
+            shellHook = ''
+          '';
+          };
+      });
+    };
+}
